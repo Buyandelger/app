5808901800ba18e84a706cdbbaf800e452b1626c
Buyandelger <buyandelger.ts@gmail.com>
2022-08-22 17:19:06 +0800

    testing additional file

---
+++ MyCustom.groovy
@@ -0,0 +1,38 @@
+def call (Map pipelineParams){
+
+    pipeline{
+        agent any
+        stages{
+            stage('Build stage'){            
+                steps{
+                    sh 'echo build starting ....'        
+                    script{
+                        node{
+                            checkout scm
+                            //docker.withRegistry('https://registry.hub.docker.com', 'dockerHub') {
+                            docker.withRegistry(pipelineParams.regUrl, 'dockerHub') {
+                                def customImage = docker.build("buyandelger/getting-started.${env.BUILD_ID}")
+                                customImage.push()
+                            }
+                        }
+                    }                          
+                }
+            }
+            stage('Deploy to test server ....'){
+                steps{
+                    script{
+                        node{
+                            def remote = [:]
+                            remote.name = 'test'
+                            remote.host = '192.168.210.131'
+                            remote.user = 'admin'
+                            remote.password = '123456'
+                            remote.allowAnyHosts = true
+                            sshCommand remote: remote, command: "docker run -d -p 3000:3000 buyandelger/getting-started.${env.BUILD_ID}"                    
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
